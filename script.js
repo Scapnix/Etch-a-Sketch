7209d9477c32507d8d352782bcc904395a7a60e2
Your Name <148256392+Scapnix@users.noreply.github.com.>
2024-02-01 21:25:21 +0200

    Edited the file a little

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let DEFAULT_SIZE = 16;
 let colors = "black";
 
-
-let slider = document.getElementById("sliderGrid")
-let sliderTxt = document.getElementById("slidertxt")
-let board = document.getElementById("container")
-let squares = document.querySelectorAll("gridB")
+let slider = document.getElementById("sliderGrid");
+let sliderTxt = document.getElementById("slidertxt");
+let board = document.getElementById("container");
+let squares = document.querySelectorAll("gridB");
 let colorValue = document.getElementById("colorMode").value;
 let color = document.getElementById("colorMode");
 let rnbwMode = document.getElementById("rnbwMode");
@@ -14,23 +13,23 @@ let clear = document.getElementById("clr");
 let items = document.querySelectorAll(".gridItem");
 
 function makeRows(size) {
+  squares.forEach((gridB) => gridB.remove());
+  {
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    container.innerHTML = "";
 
-  squares.forEach((gridB) => gridB.remove()); {
-  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-  container.innerHTML = '';
-
-let amount = size * size;
-  for (let i = 0; i < amount; i++) {
-    let cell = document.createElement("div");
-    cell.addEventListener("mouseover", changeColor)
-    cell.backgroundColor = "white";
-    container.appendChild(cell).className = "gridItem";
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+      let cell = document.createElement("div");
+      cell.addEventListener("mouseover", changeColor);
+      cell.backgroundColor = "white";
+      container.appendChild(cell).className = "gridItem";
+    }
   }
 }
-}
 
-slider.addEventListener('input', function() {
+slider.addEventListener("input", function () {
   let sliderValue = slider.value;
   sliderTxt.textContent = slider.value + "x" + slider.value;
 
@@ -38,25 +37,27 @@ slider.addEventListener('input', function() {
 });
 
 // makes 16x16 grid
-makeRows(DEFAULT_SIZE, DEFAULT_SIZE); 
+makeRows(DEFAULT_SIZE, DEFAULT_SIZE);
 
-color.addEventListener('input', function changeDraw() {
-  colors = color.value
-})
+color.addEventListener("input", function changeDraw() {
+  colors = color.value;
+});
 
 function changeColor() {
   if (colors === "random") {
-  this.style.backgroundColor = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
-
-  }else {
-  this.style.backgroundColor = colors;
+    this.style.backgroundColor =
+      "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
+  } else {
+    this.style.backgroundColor = colors;
   }
 }
 
-function changeMode(choise) {
-  colors = choise;
+function changeMode(choice) {
+  colors = choice;
 }
 
 function clearBoard() {
-  document.querySelectorAll(".gridItem").forEach((gridB) => gridB.style.backgroundColor = "white");
+  document
+    .querySelectorAll(".gridItem")
+    .forEach((gridB) => (gridB.style.backgroundColor = "white"));
 }
